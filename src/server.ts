880ffe0e3471cd6ac32e7b2a57b29900a01aef0f
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-07-09 16:05:41 -0300

    config: deploy heroku

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import 'reflect-metadata'
 import express from 'express';
 import { router } from './routes';
-
 import './container'
-const app = express();
-const port = 5000;
 
+const app = express();
+const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(router)
 
