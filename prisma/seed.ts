65e1f0bf8423e488c74098996a5f98aaa06b844a
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-07-10 16:29:25 -0300

    seed db compras com datetime

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -10,7 +10,7 @@ async function main() {
             data: {
                 clienteId:"bb409a8a-718e-4d8e-b284-4f1a0a368170",
                 produtosId:"dd5f8cfb-831f-4aef-a2dc-4307f6504d9d",
-                data_compra:"2022-01-01 23:45:12.023",
+                data_compra:faker.datatype.datetime({min:1640995200, max:1643328000}),
             }
         })
 
@@ -18,7 +18,7 @@ async function main() {
             data: {
                 clienteId:"bb409a8a-718e-4d8e-b284-4f1a0a368170",
                 produtosId:"dd5f8cfb-831f-4aef-a2dc-4307f6504d9d",
-                data_compra:"2022-02-01 23:45:12.023",
+                data_compra:faker.datatype.datetime({min:1641081600, max:1646006400}),
             }
         })
 
@@ -26,7 +26,7 @@ async function main() {
             data: {
                 clienteId:"bb409a8a-718e-4d8e-b284-4f1a0a368170",
                 produtosId:"d38bb89a-3256-4275-b871-4121050b750e",
-                data_compra:"2022-03-01 23:45:12.023",
+                data_compra:faker.datatype.datetime({min:1646092800, max:1648425600}),
             }
         })
 
@@ -34,7 +34,7 @@ async function main() {
             data: {
                 clienteId:"bb409a8a-718e-4d8e-b284-4f1a0a368170",
                 produtosId:"25354cd6-0d3a-473d-88ff-156d8cfaf000",
-                data_compra:"2022-04-01 23:45:12.023",
+                data_compra:faker.datatype.datetime({min:1651104000, max:1648771200}),
             }
         })
 
@@ -42,7 +42,7 @@ async function main() {
             data: {
                 clienteId:"bb409a8a-718e-4d8e-b284-4f1a0a368170",
                 produtosId:"260d5cf2-136e-42bd-b847-c060afbeba89",
-                data_compra:"2022-05-01 23:45:12.023",
+                data_compra:faker.datatype.datetime({min:1640995200, max:1643328000}),
             }
         })
 
@@ -50,7 +50,7 @@ async function main() {
             data: {
                 clienteId:"bb409a8a-718e-4d8e-b284-4f1a0a368170",
                 produtosId:"30396dc9-ef4e-444a-ba85-7febea448c90",
-                data_compra:"2022-06-01 23:45:12.023",
+                data_compra:faker.datatype.datetime({min:1651363200, max:1653696000}),
             }
         })
 
@@ -58,7 +58,7 @@ async function main() {
             data: {
                 clienteId:"bb409a8a-718e-4d8e-b284-4f1a0a368170",
                 produtosId:"47301a72-f80b-4ce6-b8b7-0b5c0e2d5639",
-                data_compra:"2022-07-01 23:45:12.023",
+                data_compra:faker.datatype.datetime({min:1654041600, max:1656374400}),
             }
         })
 
@@ -66,7 +66,7 @@ async function main() {
             data: {
                 clienteId:"bb409a8a-718e-4d8e-b284-4f1a0a368170",
                 produtosId:"4cb8c5d7-d1bb-4de2-8774-09a8cdc4a5e7",
-                data_compra:"2022-01-01 23:45:12.023",
+                data_compra:faker.datatype.datetime({min:1656633600,max:1658966400}),
             }
         })
 
@@ -74,7 +74,7 @@ async function main() {
             data: {
                 clienteId:"bb409a8a-718e-4d8e-b284-4f1a0a368170",
                 produtosId:"6761bdd6-dde9-4377-b01b-f0a0c3dbcaf0",
-                data_compra:"2022-01-01 23:45:12.023",
+                data_compra:faker.datatype.datetime({min:1640995200, max:1643328000}),
             }
         })
 
@@ -82,7 +82,7 @@ async function main() {
             data: {
                 clienteId:"bb409a8a-718e-4d8e-b284-4f1a0a368170",
                 produtosId:"709df709-744b-48b4-8e52-8b4ad576a528",
-                data_compra:"2022-01-01 23:45:12.023",
+                data_compra:faker.datatype.datetime({min:1640995200, max:1643328000}),
             }
         })
     }
