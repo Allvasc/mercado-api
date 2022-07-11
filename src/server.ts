9a712e3aa87c9c4753116bf03e0822ecf87c0afb
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-07-11 14:12:52 -0300

    cors config

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,14 @@ import 'reflect-metadata'
 import express from 'express';
 import { router } from './routes';
 import './container'
+import cors from 'cors'
 
 const app = express();
+app.use(cors())
 const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(router)
+
 
 app.listen(port, () => {
     console.log(`O servidor está rodando na porta ${port}`);
